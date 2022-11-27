7898965a89f9acc9b2c5c393bf6da4fa05685983
Ibrahim_Adel <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2022-11-27 21:54:49 +0200

    Last_version

---
+++ cmake-build-debug/CMakeFiles/Tiny-Language-s-Compiler.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tiny-Language-s-Compiler.dir/main.cpp.obj"
+  "CMakeFiles/Tiny-Language-s-Compiler.dir/main.cpp.obj.d"
+  "Tiny-Language-s-Compiler.exe"
+  "Tiny-Language-s-Compiler.exe.manifest"
+  "Tiny-Language-s-Compiler.pdb"
+  "libTiny-Language-s-Compiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tiny-Language-s-Compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tiny-Language-s-Compiler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tiny-Language-s-Compiler.
+# This may be replaced when dependencies are built.
