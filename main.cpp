b15e2080157221d8adf36219db99f817de8cf717
Ibrahim_Adel <84779649+Ibrahim-Adel-20190008@users.noreply.github.com>
2022-11-27 21:54:28 +0200

    Last_version

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include "scanner.cpp"
 #include "parser.cpp"
-
 using namespace std;
-
 int main() {
+    CompilerInfo compiler("input.txt", "output.txt", "debug.txt");
 
+    cout << "Start main()\n";
 
+    TreeNode *parseTree = Program(&compiler);
+    PrintTree(parseTree);
+    fflush(NULL);
 
+    cout << "End main()\n";
 
+    return 0;
     return 0;
 }
